638d885f2eb1c2c690a1840549f4c17bd44eae75
WangYong <wangyong_cn@foxmail.com>
2024-09-03 14:42:55 +0800

    add desc column in tbl_info and embed it ,finally get the cosine distant with question, it has better result by glance.

--- code/models/question/v_similarity.sql
+++ code/models/question/v_similarity.sql
@@ -14,6 +14,7 @@ qvt as (
         data_source,
         model_name,
         meta_vector,
+        description_vector,
         sample_vector 
     from 
         {{ref('t_tbl_emb')}}
@@ -23,8 +24,11 @@ qvt as (
         qvt.id as qid,
 		dvt.id as did,
         -- qvt.vector <+> dvt.meta_vector as meta_l1_distance,
+        
 		qvt.vector <-> dvt.meta_vector as meta_l2_distance,
 		qvt.vector <=> dvt.meta_vector as meta_cosine_distance,
+		qvt.vector <-> dvt.description_vector as desc_l2_distance,
+		qvt.vector <=> dvt.description_vector as desc_cosine_distance,
         -- qvt.vector <+> dvt.sample_vector as sample_l1_distance,
 		qvt.vector <-> dvt.sample_vector as sample_l2_distance,
 		qvt.vector <=> dvt.sample_vector as sample_cosine_distance,
@@ -39,19 +43,22 @@ select
     qid,
     did, 
     ROW_NUMBER() OVER (partition by qid ORDER BY sample_cosine_distance) AS sample_distance_rank,
+    ROW_NUMBER() OVER (partition by qid ORDER BY desc_cosine_distance) AS desc_distance_rank,
     ROW_NUMBER() OVER (partition by qid ORDER BY meta_cosine_distance) AS meta_distance_rank,
-    round(meta_cosine_distance::numeric,2) as meta_cosine_distance,
-    round(sample_cosine_distance::numeric,2) as sample_cosine_distance,
-    round(meta_l2_distance::numeric,2) as meta_l2_distance,
-    round(sample_l2_distance::numeric,2) as sample_l2_distance,
+    round(meta_cosine_distance::numeric,3) as meta_cosine_distance,
+    round(desc_cosine_distance::numeric,3) as desc_cosine_distance,
+    round(sample_cosine_distance::numeric,3) as sample_cosine_distance,
+    round(meta_l2_distance::numeric,3) as meta_l2_distance,
+    round(desc_l2_distance::numeric,3) as desc_l2_distance,
+    round(sample_l2_distance::numeric,3) as sample_l2_distance,
     model_name
 from
     distance t  
 )
 select 
     qid, did, 
-    meta_distance_rank, sample_distance_rank,
-    meta_cosine_distance, sample_cosine_distance
+    meta_distance_rank, desc_distance_rank, sample_distance_rank,
+    meta_cosine_distance, desc_cosine_distance, sample_cosine_distance
 from ranked
 where meta_distance_rank <=3
-order by qid, meta_distance_rank,sample_distance_rank
+order by qid, meta_distance_rank,desc_distance_rank
--- code/models/table_meta/t_tbl_emb.sql
+++ code/models/table_meta/t_tbl_emb.sql
@@ -15,7 +15,8 @@ emb_model as (
         column_info,
         table_sample,
         schema_name ||'.'||table_name || column_info as data_meta,
-        schema_name ||'.'||table_name || column_info || table_sample as data_sample
+        schema_name ||'.'||table_name || column_info || table_sample as data_sample,
+        data_description
     from {{ref('t_tbl_info')}}
 )
 select 
@@ -23,6 +24,7 @@ select
     schema_name || table_name as data_source,
     name as model_name,
     ollama_embed(name, data_meta ) as meta_vector ,
-    ollama_embed(name, data_sample) as sample_vector 
+    ollama_embed(name, data_sample) as sample_vector,
+    ollama_embed(name, data_description ) as description_vector 
 from
    emb_model cross join tbl_info
